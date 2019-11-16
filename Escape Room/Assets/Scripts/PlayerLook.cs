8a180d1ace040c5b16fe52e778528db8aa8398dd
kpeenee <43548093+kpeenee@users.noreply.github.com>
2019-11-16 16:29:02 +0000

    Player Movement

---
+++ Escape Room/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] float lookSensitivity = 150;
+    [SerializeField] float maxClamp = 90f;
+    [SerializeField] float minClamp = -90f;
+
+    private float xAxisClamp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.right);
+        xAxisClamp = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotate();
+    }
+
+    private void CameraRotate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * lookSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        xAxisClamp += mouseY;
+        xAxisClamp = Mathf.Clamp(xAxisClamp, minClamp, maxClamp);
+
+       
+        if(xAxisClamp >= maxClamp)
+        {
+            mouseY = 0.0f;
+            transform.eulerAngles = SetCameraRotation(maxClamp + 180);
+        }
+        else if(xAxisClamp <= minClamp)
+        {
+            mouseY = 0.0f;
+            transform.eulerAngles = SetCameraRotation(minClamp + 180);
+        }
+
+        transform.Rotate(-Vector3.right * mouseY);
+        transform.parent.Rotate(Vector3.up * mouseX);
+        
+    }
+
+    private Vector3 SetCameraRotation(float value)
+    {
+        Vector3 curretnRotation = transform.eulerAngles;
+        curretnRotation.x = value;
+        return curretnRotation;
+    }
+}
---
+++ Escape Room/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        Vector3 foward = transform.forward * inputY * moveSpeed;
+        Vector3 side = transform.right * inputX * moveSpeed;
+        Vector3 up = new Vector3(0, rb.velocity.y, 0);
+
+        rb.velocity = foward + side + up;
+       
+    }
+}
