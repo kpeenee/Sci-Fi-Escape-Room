b9fcca456874106f2c47b2b0ad3f680e8317fb33
kpeenee <43548093+kpeenee@users.noreply.github.com>
2019-12-13 07:41:19 +0000

    Main Menu

---
+++ Escape Room/Assets/Scripts/Scroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    Material material;
+    [SerializeField] float speed = 1.0f;
+    Vector2 offsetChange;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offsetChange = new Vector2(speed, 0f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += offsetChange * Time.deltaTime;
+    }
+}
