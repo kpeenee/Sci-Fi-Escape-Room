e970962e30b2b0bc4ef4ee616e06107d57e5f986
kpeenee <43548093+kpeenee@users.noreply.github.com>
2020-01-02 19:09:12 +0000

    Computer open and closes door

---
+++ Escape Room/Assets/Computer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Computer : MonoBehaviour, Iinteractable
+{
+    private string show = "Computer";
+    [SerializeField] private GameObject interactWith;
+
+    public void Interact()
+    {
+        Animator door = interactWith.GetComponent<Animator>();
+        if (door.GetBool("isOpen"))
+        {
+            Debug.Log("Opening");
+            door.SetBool("isOpen", false);
+        }
+        else
+        {
+            Debug.Log("Closing");
+            door.SetBool("isOpen", true);
+        }
+        
+    }
+}
---
+++ Escape Room/Assets/FindInteract.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindInteract : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] LayerMask interactableLayer;
+    [SerializeField] float distance = 1f;
+
+    Iinteractable currentInteractable;
+
+    private void Update()
+    {
+        LookForObject();
+        Interact();
+    }
+
+    private void Interact()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentInteractable != null)
+            {
+                Debug.Log("Yep");
+                currentInteractable.Interact();
+            }
+        }
+    }
+
+    private void LookForObject()
+    {
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray,out hitInfo, distance, interactableLayer))
+        {
+            currentInteractable = hitInfo.collider.GetComponent<Iinteractable>();
+            Debug.Log("ItemHit");
+        }
+        else
+        {
+            currentInteractable = null;
+            Debug.Log("NoItem");
+        }
+    }
+}
