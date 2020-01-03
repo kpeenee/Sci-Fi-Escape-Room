fdaa0c2a3e1af1d12160c89069b0c4e814845189
kpeenee <43548093+kpeenee@users.noreply.github.com>
2020-01-03 17:29:16 +0000

    Puzzle 1

---
+++ Escape Room/Assets/Scripts/Beaker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beaker : MonoBehaviour, Iinteractable
+{
+    [SerializeField] int currentValue = -2;
+    [SerializeField] Material[] materials;
+    [SerializeField] GameObject liquid;
+    [SerializeField] GameObject key;
+
+    public void Interact()
+    {
+        if(currentValue == 0)
+        {
+            Destroy(key);
+        }
+    }
+
+    public void ChangeCurrent(int value)
+    {
+        currentValue += value;
+        if(currentValue < -2)
+        {
+            currentValue = -2;
+        }
+        else if(currentValue > 2)
+        {
+            currentValue = 2;
+        }
+
+        ChangeColour();
+    }
+
+    private void ChangeColour()
+    {
+        liquid.GetComponent<Renderer>().material = materials[currentValue + 2];
+    }
+}
---
+++ Escape Room/Assets/Scripts/Flasks.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flasks : MonoBehaviour, Iinteractable
+{
+    [SerializeField] int value = 0;
+    [SerializeField] Beaker beaker;
+    public void Interact()
+    {
+        beaker.ChangeCurrent(value);
+    }
+}
